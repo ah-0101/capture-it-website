337ca51cfff46bb2ce43d865e86453635a5059fa
Ace Habes <72156174+Ace-0101@users.noreply.github.com>
2021-02-23 12:26:27 -0800

    home-page-loggedout

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import { Redirect } from 'react-router-dom';
 import './Navigation.css';
-
+import logo from './logo.png'
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
 
@@ -19,20 +19,34 @@ function Navigation({ isLoaded }){
   } else {
     sessionLinks = (
       <>
-        <NavLink to="/login">Log In</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
+        <NavLink to="/login" className='log'>Log In</NavLink>
+        <NavLink to="/signup" className='sign'>Sign Up</NavLink>
       </>
     );
   }
 
   return (
+    <header>
+      <NavLink exact to="/" className='nav'>
+      <img className='logo' src={logo} alt="logo"/>
+      </NavLink>
+
+
+      <p className='captureIt'>CaptureIt</p>
     <ul>
-      <li>
-        <NavLink exact to="/">Home</NavLink>
+      <li className='logo-nav' >
+        
         {isLoaded && sessionLinks}
       </li>
     </ul>
+    </header>
   );
 }
 
 export default Navigation;
+
+
+{/* <li className='logo-container'>
+<img className='logo' src={logo} alt="logo"/>
+
+</li> */}
